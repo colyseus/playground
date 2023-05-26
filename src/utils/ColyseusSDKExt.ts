7e8734efd5732e4ae2bc690d3bb392192e0d26e3
Endel Dreyer <endel.dreyer@gmail.com>
2023-05-26 17:19:56 -0300

    fix re-binding reconnect events

--- src/utils/ColyseusSDKExt.ts
+++ src/utils/ColyseusSDKExt.ts
@@ -6,6 +6,44 @@ import { Room, Protocol } from "colyseus.js";
 export const RAW_EVENTS_KEY = '$_raw';
 export const DEVMODE_RESTART = '$_devmode';
 
+const connect = Room.prototype['connect'];
+Room.prototype['connect'] = function(endpoint: string, devModeCloseCallback: () => void, room: Room) {
+  // @ts-ignore
+  connect.apply(this, arguments);
+
+  (room as any)[RAW_EVENTS_KEY] = [];
+
+  // intercept send events
+  const send = room.connection.transport['send'];
+  room.connection.transport['send'] = (data: ArrayBuffer) => {
+    const sendBytes = Array.from(new Uint8Array(data));
+
+    room['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['out', getEventType(sendBytes[0]), sendBytes]);
+    send.call(room.connection.transport, data);
+  };
+
+  const ws = (room.connection.transport as any).ws;
+
+  const onerror = ws.onerror;
+  ws.onerror = (error: any) => {
+    room['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['error', 'ERROR', error.message]);
+    onerror(error);
+  };
+
+  // intercept close events
+  const onclose = ws.onclose;
+  ws.onclose = (event: any) => {
+    delete (room as any)[RAW_EVENTS_KEY];
+    if (event.code === 4010) {// CloseCode.DEVMODE_RESTART
+      room['onMessageHandlers'].emit(DEVMODE_RESTART);
+      room['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['close', 'CLOSE_DEVMODE_RESTART', { code: event.code }]);
+    } else {
+      room['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['close', 'CLOSE', { code: event.code }]);
+    }
+    onclose(event);
+  };
+}
+
 const onMessageCallback = Room.prototype['onMessageCallback'];
 Room.prototype['onMessageCallback'] = function(event: MessageEvent) {
   const bytes = Array.from(new Uint8Array(event.data))
@@ -13,39 +51,6 @@ Room.prototype['onMessageCallback'] = function(event: MessageEvent) {
   // create local cache while the room is joining.
   // so we can consume them immediately when the join callback is called.
   if (!this['onMessageHandlers'].events[RAW_EVENTS_KEY]) {
-    if (!(this as any)[RAW_EVENTS_KEY]) {
-      (this as any)[RAW_EVENTS_KEY] = [];
-
-      // intercept send events
-      const originalSend = this.connection.transport['send'];
-      this.connection.transport['send'] = (data: ArrayBuffer) => {
-        const sendBytes = Array.from(new Uint8Array(data));
-
-        this['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['out', getEventType(sendBytes[0]), sendBytes]);
-        originalSend.call(this.connection.transport, data);
-      };
-
-      const ws = (this.connection.transport as any).ws;
-
-      const onError = ws.onerror;
-      ws.onerror = (error: any) => {
-        this['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['error', 'ERROR', error.message]);
-        onError(error);
-      };
-
-      // intercept close events
-      const onClose = ws.onclose;
-      ws.onclose = (event: any) => {
-        if (event.code === 4010) {// CloseCode.DEVMODE_RESTART
-          this['onMessageHandlers'].emit(DEVMODE_RESTART);
-          this['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['close', 'CLOSE_DEVMODE_RESTART', { code: event.code }]);
-
-        } else {
-          this['onMessageHandlers'].emit(RAW_EVENTS_KEY, ['close', 'CLOSE', { code: event.code }]);
-        }
-        onClose(event);
-      };
-    }
     (this as any)[RAW_EVENTS_KEY].unshift(['in', getEventType(bytes[0]), bytes, new Date()]);
   }
 
