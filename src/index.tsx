a4dc0418df3b5926e079a31bda6c3f7e507d025f
Endel Dreyer <endel.dreyer@gmail.com>
2023-06-02 14:10:27 -0300

    use HashRouter in order to use subdirectories

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
